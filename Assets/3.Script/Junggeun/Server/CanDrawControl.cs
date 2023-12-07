9fb75ec40bef7ac1ff8dc2618909ff51bc732f4d
ssbaring <lldgll@naver.com>
2023-12-07 14:40:54 +0900

    [Feat] DrawSync

--- Assets/3.Script/Junggeun/Server/CanDrawControl.cs
+++ Assets/3.Script/Junggeun/Server/CanDrawControl.cs
@@ -9,11 +9,15 @@ public class CanDrawControl : NetworkBehaviour
     [SyncVar(hook = nameof(onCanDrawChanged))]
     public bool isCanDraw;
 
-    GameObject img;
+    [SerializeField] GameObject img;
+    [SerializeField] GameObject panel;
+
+
 
     private void Start()
     {
-        img = GameObject.FindGameObjectWithTag("Finish");
+        //img = GameObject.FindGameObjectWithTag("Finish");
+        //panel = GameObject.FindGameObjectWithTag("Respawn");
         GameObject[] a = GameObject.FindGameObjectsWithTag("Player");
         if (a.Length <= 1)
         {
@@ -24,27 +28,41 @@ public class CanDrawControl : NetworkBehaviour
             ChangeCanDraw2(false);
         }
     }
+
     private void Update()
     {
         img.SetActive(!isCanDraw);
+        panel.SetActive(isCanDraw);
     }
 
     private void onCanDrawChanged(bool _old, bool _new)
     {
-        Debug.Log("onCanDrawChanged");
+        Debug.Log($"{_new}");
         isCanDraw = _new;
     }
 
     public void ChangeCanDraw2(bool t)
     {
-        Debug.Log($"ChangeCanDraw2{t}");
+        //Debug.Log($"ChangeCanDraw2{t}");
         ChangeCanDraw(t);
+        
+        if (img == null)
+        {
+            img = GameObject.FindGameObjectWithTag("Finish").transform.GetChild(1).gameObject;
+        }
+        if (panel == null)
+        {
+            panel = GameObject.FindGameObjectWithTag("Respawn").transform.GetChild(0).gameObject;
+        }
+
+        //Debug.Log($"Change Can Draw2 에서는 {isCanDraw}");
+
     }
 
     [Command]
     public void ChangeCanDraw(bool t)
     {
-        Debug.Log($"ChangeCanDraw{t}");
         isCanDraw = t;
+        //Debug.Log($"Change Can Draw 에서는 {isCanDraw}");
     }
 }
